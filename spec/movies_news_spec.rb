1258084636aec7af13e33cdefd39a4c070b9d960
phaltoe <reverse9@live.com>
2016-08-07 06:57:29 -0300

    deleted unnecessary tests

--- spec/movies_news_spec.rb
+++ spec/movies_news_spec.rb
@@ -4,8 +4,4 @@ describe MoviesNews do
   it 'has a version number' do
     expect(MoviesNews::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
